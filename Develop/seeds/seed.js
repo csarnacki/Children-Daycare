ae749e39438c357fb7781c248503432af41411af
Chris <sirchrissarnacki@gmail.com>
2023-07-19 20:15:21 -0700

    Created seed.js file and added seed script to package.json

---
+++ Develop/seeds/seed.js
@@ -0,0 +1,31 @@
+const sequelize = require('./config.connect');
+const { Child, Contact, User } = require('../models');
+
+const childData = require('./childData.json');
+const contactData = require('./contactData.json');
+const userData = require('./userData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    const users = await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    const children = await Child.bulkCreate(childData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    for (const contact of contactData) {
+        await Contact.create({
+            ...contact,
+            child_id: children[Math.floor(Math.random() * children.length)].id,
+        });
+    }
+
+    process.exit(0);
+};
+
+seedDatabase();
