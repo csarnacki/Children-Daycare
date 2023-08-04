486e410fd2e64a6bc8e880789a43e846fc69bf90
Chris <sirchrissarnacki@gmail.com>
2023-08-03 18:54:57 -0700

    Cleaned up get route in home route and changed main handlebars layout

--- Develop/controllers/homeRoutes.js
+++ Develop/controllers/homeRoutes.js
@@ -2,19 +2,8 @@ const router =require('express').Router();
 const { User, Child, Contact } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', async (req, res) => {
-    try {
-        const childData = await Child.findAll({});
-
-        const children = childData.map((child) => child.get({ plain: true }));
-
-        res.render('homepage', {
-            children,
-            logged_in: req.session.logged_in
-        });
-    }   catch (err) {
-        res.status(500).json(err);
-    }
+router.get('/', (req, res) => {
+   res.render('homepage', { layout: 'main', loggedIn: req.session.logged_in });
 });
 
 router.get('/child/:id', async (req, res) => {
