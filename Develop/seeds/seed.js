521b60c07bde080cdcf854e58b4d4ea2dfc317b2
Chris <sirchrissarnacki@gmail.com>
2023-08-07 19:47:29 -0700

    Updated seed.js file and tested seeded data

--- Develop/seeds/seed.js
+++ Develop/seeds/seed.js
@@ -1,27 +1,21 @@
 const sequelize = require('../config/connect');
-const { Child, Contact, User } = require('../models');
+const { Game, User } = require('../models');
 
-const childData = require('./childData.json');
-const contactData = require('./contactData.json');
+const gameData = require('./gameData.json');
 const userData = require('./userData.json');
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
-    await User.bulkCreate(userData, {
+    const users = await User.bulkCreate(userData, {
         individualHooks: true,
         returning: true,
     });
 
-    const children = await Child.bulkCreate(childData, {
-        individualHooks: true,
-        returning: true,
-    });
-
-    for (const contact of contactData) {
-        await Contact.create({
-            ...contact,
-            child_id: children[Math.floor(Math.random() * children.length)].id,
+    for (const game of gameData) {
+        await Game.create({
+            ...game,
+            user_id: users[Math.floor(Math.random() * users.length)].id,
         });
     }
 
