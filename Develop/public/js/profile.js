8a55b2d2e1818d7e26cef5b7ce42eff046ba5e5a
Chris <sirchrissarnacki@gmail.com>
2023-08-12 19:57:58 -0700

    Created profile hs page for user to add and delete games from their list

---
+++ Develop/public/js/profile.js
@@ -0,0 +1,45 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+
+    const name = document.querySelector('#game-name').value.trim();
+    const weeks_playing = document.querySelector('#weeks-playing').value.trim();
+    const genre = document.querySelector('#genre').value.trim();
+
+    if (name && weeks_playing && genre) {
+        const response = await fetch('/routes/games', {
+            method: 'POST',
+            body: JSON.stringify({ name, weeks_playing, genre }),
+            headers: { 'Content-Type': 'application/json'},
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Failed to create game list!');
+        }
+    };
+};
+
+const deleteButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+        const id = event.target.getAttribute('data-id');
+
+        const response = await fetch(`/routes/games${id}`, {
+            method: 'DELETE',
+        });
+
+        if (response.ok) {
+            document.location.replace('/profile');
+        } else {
+            alert('Failed to delete game list!');
+        }
+    }
+};
+
+document
+    .querySelector('.new-game-list')
+    .addEventListener('submit', newFormHandler);
+
+document
+    .querySelector('.game-list')
+    .addEventListener('click', deleteButtonHandler);
